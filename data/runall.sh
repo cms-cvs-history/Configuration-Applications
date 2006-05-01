5c825dfd17f9d2cfd0162cd9a7170dcb8a581d13
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2006-05-01 14:04:59 +0000

    runall.sh for different tests

--- data/runall.sh
+++ data/runall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #
-#$Id: runall.sh,v 1.1 2006/04/19 20:37:38 argiro Exp $
+#$Id: runall.sh,v 1.2 2006/04/25 10:28:19 argiro Exp $
 #
 #Dummy script to run all integration tests
 #
 #
 
-tests="reco-application-sim_10mum.cfg
+testsRS="reco-application-sim_10mum.cfg
 reco-application-digitize_strips_from_flatptgun.cfg
 reco-application-DigisToClusters.cfg
 reco-application-ClustersToHits.cfg
@@ -17,14 +17,32 @@ reco-application-CloudsToClean.cfg
 reco-application-CleanToTrackCandidates.cfg
 reco-application-TrackCandidatesToTracks.cfg
 reco-application-AnalyzeTracks.cfg
-sim-application.cfg
+"
+
+
+testsKF="reco-application-KF-generation.cfg
+reco-application-KF-digitization.cfg
+reco-application-KF-rechits.cfg
+reco-application-KF-seeds.cfg
+reco-application-KF-trajectorybuilding.cfg
+reco-application-KF-finalfit.cfg
+reco-application-KF-readtracks.cfg
+"
+
+
+testsSim="sim-application.cfg
 sim-application-digitization.cfg
 "
+
+tests=`echo $testsKF $othertests testsSim`
+
+echo "Tests to be run : " $tests
 
 eval `scramv1 runtime -sh`
 
 for file in $tests 
 do
+    echo Preparing to run $file
     cmsRun $file
     if [ $? -ne 0 ] ;then
       echo "cmsRun $file : FAILED"
