75fa78d2e3e0055640dc879bf68f5cfce89f7788
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2006-06-07 22:18:54 +0000

    added tests for ecal, csc, vertex

--- data/runall.sh
+++ data/runall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-#$Id: runall.sh,v 1.8 2006/06/06 15:56:50 argiro Exp $
+#$Id: runall.sh,v 1.9 2006/06/07 13:42:56 argiro Exp $
 #
 #Dummy script to run all integration tests
 #
@@ -31,7 +31,17 @@ testsMuon="recolocal-application-dtrechits.cfg
 recolocal-application-cscchain.cfg
 "
 
-tests=`echo $testsTracking $testsSim $testsMuon`
+testVertex="reco-application-primaryvertexfinding.cfg"
+
+testEcal="reco-application-ecal-simulation.cfg
+reco-application-ecal-digitization.cfg
+reco-application-ecal-localreco.cfg
+reco-application-ecal-egammaclusters.cfg
+"
+
+testHcal="Zprime_Dijets_2000_JetMETSimReco.cfg"
+
+tests=`echo $testsTracking $testsSim $testsMuon $testVertex $testEcal $testHcal`
 
 report=""
 
